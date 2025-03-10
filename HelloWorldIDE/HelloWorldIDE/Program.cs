58c3cf4f6b483a2a338aacf1fd5006fd6d45d863
Sohomz <sohomd088@gmail.com>
2025-03-10 13:53:47 +0530

    2nd time commit

--- HelloWorldIDE/HelloWorldIDE/Program.cs
+++ HelloWorldIDE/HelloWorldIDE/Program.cs
@@ -1,3 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.ReadLine();
+﻿//Console.WriteLine("Hello, World!");
+//Console.ReadLine();
+
+//int age = 25;
+//long num = 24444444444444;
+//float pi = 3.14f;
+//double d = 3.123464445;
+//char c = 'a';
+//String s = "sohom";
+//byte b = 23;
+//sbyte sb = -30;
+//short sh = -345;
+//ushort ush = 200;
+//Console.WriteLine($"age:{age} \nLong num:{num} \nFloat pi:{pi} \nDouble num:{d} \nCharacter:{c} \nString:{s} \nByte num:{b} \nsByte num:{sb} \nshort Num:{sh} \nUnsigned short Num:{ush}");
+
+//String firstName = "Sohom";
+//String lastName = "Das";
+//Console.WriteLine(firstName + " " + lastName);
+
+int employeeId = 20;
+String companyName = "UTCLI";
+String lowerCompanyName = companyName.ToLower();
+String subStringcompanyName = companyName.Substring(2);
+
+Console.WriteLine($"Hello, my employee ID is {employeeId} and my company name is {companyName} {lowerCompanyName} {subStringcompanyName}");
+
--- HelloWorldIDE/HelloWorldIDE/obj/Debug/net8.0/HelloWorldIDE.AssemblyInfo.cs
+++ HelloWorldIDE/HelloWorldIDE/obj/Debug/net8.0/HelloWorldIDE.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorldIDE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf2144ec791c1db830b6895ca3b99590a7e6f0a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorldIDE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorldIDE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
